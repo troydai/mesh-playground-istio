8f7c1231adc2eefbf3c2e353c11888c940ed3939
Troy Dai <git@troydai.com>
2023-01-08 23:41:58 -0800

    basic setup

---
+++ scripts/k8s/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CLUSTER_NAME=mesh-playground-istio
+
+kubectl config use-context "kind-$CLUSTER_NAME"
+
+# Install Istio (https://istio.io/latest/docs/setup/getting-started/#download)
+
+# Set up kubenetes dashboard
+kubectl apply -f ./config/k8s/dashboard.yaml
+kubectl create serviceaccount -n kubernetes-dashboard admin-user
+kubectl create clusterrolebinding \
+    -n kubernetes-dashboard \
+    admin-user \
+    --clusterrole cluster-admin \
+    --serviceaccount=kubernetes-dashboard:admin-user
